6455402e3ebbfe9115d285ac90a1fe74372ba265
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-07-31 23:16:15 +0530

    Added title and edited icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
 import Navbar from './Navbar'
 const Header = () => {
-    const headerTitle = "Title"
+    const headerTitle = "Pixel"
   return (
     <header>
         <div className=' border-b mb-2 md:mb-5'>
             <div className='flex items-center justify-between py-2 md:py-4 mx-3 md:mx-10'>
-                <h2 className='text-2xl font-semibold'>{headerTitle}</h2>
-                <GiHamburgerMenu className='text-red-700'/>
+                <h2 className='text-2xl font-semibold cursor-pointer'>{headerTitle}<span className='text-red-600'>6</span></h2>
+                <GiHamburgerMenu className='text-red-700 scale-110 hover:scale-125 duration-300 cursor-pointer'/>
             </div>
         </div>
         <Navbar/>
