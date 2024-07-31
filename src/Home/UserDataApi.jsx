138f876ce3d18f0a74e4489abc1059ec85d6ef39
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-07-31 23:12:56 +0530

    Added .env file for api security

--- src/Home/UserDataApi.jsx
+++ src/Home/UserDataApi.jsx
@@ -2,50 +2,52 @@ import React, { useEffect, useState } from "react";
 import UserDetail from "./UserDetail";
 
 const UserDataApi = () => {
-    const [data, setData] = useState([]);
-    const [page, setPage] = useState(2);
-    const [loading, setLoading] = useState(false);
+     const [data, setData] = useState([]);
+     const [page, setPage] = useState(1);
+     const [loading, setLoading] = useState(false);
 
-    // FOR FETCHING API DATA
-    useEffect(() => {
-        const fetchApiData = async () => {
-            setLoading(true);
-            try {
-                const res = await fetch(`https://dummyjson.com/users?`);
-                const data = await res.json();
-                setData(prev => [...prev, ...data.users]);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            } finally {
-                setLoading(false);
-            }
-        };
+     const apiUrl = import.meta.env.VITE_API_URL;
 
-        fetchApiData();
-    }, [page]);
+     // FOR FETCHING API DATA
+     useEffect(() => {
+          const fetchApiData = async () => {
+               setLoading(true);
+               try {
+                    const res = await fetch(apiUrl);
+                    const data = await res.json();
+                    setData((prev) => [...prev, ...data.users]);
+               } catch (error) {
+                    console.error("Error fetching data:", error);
+               } finally {
+                    setLoading(false);
+               }
+          };
 
-    // FOR HANDLING THE INFINTE SCROLL
-    useEffect(() => {
-        const handleInfiniteScroll = () => {
-            if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
-                if (!loading) {
-                    
-                }
-            }
-        };
+          fetchApiData();
+     }, [page]);
 
-        window.addEventListener("scroll", handleInfiniteScroll);
-        return () => {
-            window.removeEventListener("scroll", handleInfiniteScroll);
-        };
-    }, [loading]);
+     // FOR HANDLING THE INFINTE SCROLL
+     //  useEffect(() => {
+     //       const handleInfiniteScroll = () => {
+     //            if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
+     //                 if (!loading) {
+     //                      setPage((prev) => prev + 1);
+     //                 }
+     //            }
+     //       };
 
-    return (
-        <div className="mx-3 md:mx-10">
-            <UserDetail data={data} />
-            {loading && <div>Loading...</div>}
-        </div>
-    );
+     //       window.addEventListener("scroll", handleInfiniteScroll);
+     //       return () => {
+     //            window.removeEventListener("scroll", handleInfiniteScroll);
+     //       };
+     //  }, [loading]);
+
+     return (
+          <div className="mx-3 md:mx-10">
+               <UserDetail data={data} />
+               {loading && <div>Loading...</div>}
+          </div>
+     );
 };
 
 export default UserDataApi;
